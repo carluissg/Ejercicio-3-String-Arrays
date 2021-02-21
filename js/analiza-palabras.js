33da7b6642d60b2c48bab41df649c6dfbd5fc3f4
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-02-21 08:24:25 +0100

    commit inicial

---
+++ js/analiza-palabras.js
@@ -0,0 +1,27 @@
+/* Crea tu función aquí */
+
+function analizaPalabras(palabras) {
+    for (const [i, palabra] of palabras.entries()) {
+        console.log("####");
+        console.log("Palabra" + [i + 1] + "= " + palabra);
+        console.log("Numero de Caracteres: " + palabra.length);
+        console.log("Deletreo: " + palabra.split("").join("_"));
+        if ((palabra.length % 2 === 0) && (palabra.charAt(0).toUpperCase())) {
+            console.log("La palabra es par y empieza por mayúscula");
+        } else if (palabra.charAt(0).toUpperCase()) {
+            console.log("La palabra es impar y empieza por mayúscula");
+        } else if ((palabra.length % 2 === 0) && (palabra.charAt(0).toLowerCase())) {
+            console.log("La palabra es par y no empieza por mayúscula");
+        } else {
+            console.log("La palabra es impar y no empieza por mayúscula");
+        }
+        if (palabra.includes("ble")) {
+            console.log("La palabra sí contiene 'ble'");
+        } else {
+            console.log("La palabra no contiene 'ble'");
+        }
+    }
+}
+
+
+
